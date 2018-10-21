cd223c8063fb01d73af6f0ef6f09414e9252150f
Paul Suarez <paulsblogging.com@gmail.com>
2018-10-20 21:38:12 -0700

    setting up character class and allowing enemy class to inherit from character

--- Nedanstad/Assets/Scripts/Character.cs
+++ Nedanstad/Assets/Scripts/Character.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour {
+namespace NedanstadRPG
+{
+    public class Character : MonoBehaviour
+    {
+        public int Energy { get; set; }
+        public int Attack { get; set; }
+        public int Defence { get; set; }
+        public int Gold { get; set; }
+        public Vector2 RoomIndex { get; set; }
+        public List<string> Inventory { get; set; }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        public virtual void TakeDamage(int amount)
+        {
+            Energy -= amount;
+            if (Energy <= 0)
+            {
+                Die(); 
+            }
+        }
+
+        public virtual void Die()
+        {
+            Debug.Log("GAME OVER");
+        }
+
+    }
 }
---
+++ Nedanstad/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NedanstadRPG;
+
+public class Enemy : Character
+{
+
+
+
+    public override void TakeDamage(int amount)
+    {
+        base.TakeDamage(amount);
+        Debug.Log("An enemy is taking damage ");
+    }
+
+    public override void Die()
+    {
+        base.Die();
+        Debug.Log("An enemy has been slain! ");
+    }
+}
